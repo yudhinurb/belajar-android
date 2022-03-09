205cd7708c8ed36207c3fab5acae70c93d728155
yudhinurb <yudhinurb@gmail.com>
2022-03-09 07:17:19 +0700

    move to main activity

--- app/src/main/java/com/yudhinurb/zwallet/ui/MainActivity.kt
+++ app/src/main/java/com/yudhinurb/zwallet/ui/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.yudhinurb.zwallet.ui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yudhinurb.zwallet.R
+import com.yudhinurb.zwallet.adapter.TransactionAdapter
+import com.yudhinurb.zwallet.data.Transaction
 
 class MainActivity : AppCompatActivity() {
+    private val transactionData = mutableListOf<Transaction>()
+    lateinit var transactionAdapter: TransactionAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_hello_world)
+        this.transactionAdapter = TransactionAdapter(transactionData)
+        val recycler: RecyclerView = findViewById(R.id.recyclerTransaction)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recycler.layoutManager = layoutManager
+        recycler.adapter = transactionAdapter
+        prepareData()
+    }
 
-        val hello = findViewById<TextView>(R.id.helloWorld)
-        hello.text = "Halo ini adalah ZWallet"
+    fun prepareData(){
+        this.transactionData.add(
+            Transaction(
+            transactionImage = getDrawable(R.drawable.rumbling)!!,
+            transactionName = "Yudhi Nur",
+            transactionType = "Transfer",
+            transactionNominal = 125000.00
+        )
+        )
 
-        val btnNavigate = findViewById<Button>(R.id.btnNavigate)
-        btnNavigate.setOnClickListener {
-            val intent = Intent(this, HelloWorld::class.java)
-            startActivity(intent)
-        }
+        this.transactionAdapter.notifyDataSetChanged()
     }
 }
