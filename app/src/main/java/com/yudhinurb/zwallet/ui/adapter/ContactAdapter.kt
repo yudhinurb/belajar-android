941c6f4c861ab8df2e07e11112db7299cbc24b57
yudhinurb <yudhinurb@gmail.com>
2022-03-17 13:50:09 +0700

    contact

--- app/src/main/java/com/yudhinurb/zwallet/ui/adapter/ContactAdapter.kt
+++ app/src/main/java/com/yudhinurb/zwallet/ui/adapter/ContactAdapter.kt
@@ -1,4 +1,54 @@
 package com.yudhinurb.zwallet.ui.adapter
 
-class ContactAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.imageview.ShapeableImageView
+import com.yudhinurb.zwallet.R
+import com.yudhinurb.zwallet.model.AllContacts
+import com.yudhinurb.zwallet.utils.BASE_URL
+
+class ContactAdapter(private var data: List<AllContacts>): RecyclerView.Adapter<ContactAdapter.ContactAdapterHolder>() {
+    private lateinit var contextAdapter: Context
+
+    class ContactAdapterHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val image: ShapeableImageView = view.findViewById(R.id.imageContact)
+        private val name: TextView = view.findViewById(R.id.contactName)
+        private val phone: TextView = view.findViewById(R.id.contactNumber)
+
+        fun bindData(data: AllContacts, context: Context, position: Int){
+            Glide.with(image).load(BASE_URL +data.image).apply(
+                RequestOptions.circleCropTransform().placeholder(R.drawable.rumbling)
+            ).into(image)
+            name.text = data.name.toString()
+            phone.text = data.phone.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ContactAdapter.ContactAdapterHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        this.contextAdapter = parent.context
+        val inflatedView: View = inflater.inflate(R.layout.item_contact_findreceiver, parent, false)
+        return ContactAdapterHolder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapter.ContactAdapterHolder, position: Int) {
+        holder.bindData(this.data[position], contextAdapter, position)
+    }
+
+    override fun getItemCount(): Int {
+        return this.data.size
+    }
+
+    fun addData(data: List<AllContacts>){
+        this.data = data
+    }
 }
--- app/src/main/java/com/yudhinurb/zwallet/ui/layout/main/findreceiver/FindReceiverFragment.kt
+++ app/src/main/java/com/yudhinurb/zwallet/ui/layout/main/findreceiver/FindReceiverFragment.kt
@@ -1,5 +1,6 @@
 package com.yudhinurb.zwallet.ui.layout.main.findreceiver
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,13 +9,18 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yudhinurb.zwallet.R
 import com.yudhinurb.zwallet.databinding.FragmentFindReceiverBinding
+import com.yudhinurb.zwallet.ui.adapter.ContactAdapter
+import com.yudhinurb.zwallet.ui.adapter.TransactionAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FindReceiverFragment : Fragment() {
     private lateinit var binding: FragmentFindReceiverBinding
+    private lateinit var contactAdapter: ContactAdapter
+    private lateinit var prefs: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +35,23 @@ class FindReceiverFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
 
+        prepareData()
+
         binding.btnBack.setOnClickListener{
             findNavController().popBackStack()
         }
 
         binding.textContacts.setOnClickListener{
             Navigation.findNavController(view).navigate(R.id.action_findReceiverFragment_to_inputAmountFragment)
+        }
+    }
+
+    fun prepareData() {
+        this.contactAdapter = ContactAdapter(listOf())
+
+        binding.recyclerContact.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = contactAdapter
         }
     }
 }
