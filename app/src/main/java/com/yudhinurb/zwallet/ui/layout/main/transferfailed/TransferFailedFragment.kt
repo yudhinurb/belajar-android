518279976d7228342396c75bfe073c734b4dd565
yudhinurb <yudhinurb@gmail.com>
2022-03-18 14:59:01 +0700

    add viewmodel in success and failed

--- app/src/main/java/com/yudhinurb/zwallet/ui/layout/main/transferfailed/TransferFailedFragment.kt
+++ app/src/main/java/com/yudhinurb/zwallet/ui/layout/main/transferfailed/TransferFailedFragment.kt
@@ -1,14 +1,34 @@
 package com.yudhinurb.zwallet.ui.layout.main.transferfailed
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.yudhinurb.zwallet.R
+import com.yudhinurb.zwallet.databinding.FragmentTransferFailedBinding
+import com.yudhinurb.zwallet.databinding.FragmentTransferSuccessBinding
+import com.yudhinurb.zwallet.ui.layout.main.MainActivity
+import com.yudhinurb.zwallet.ui.layout.main.findreceiver.ContactViewModel
+import com.yudhinurb.zwallet.utils.BASE_URL
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class TransferFailedFragment : Fragment() {
+    private lateinit var binding: FragmentTransferFailedBinding
+    private val viewModel: ContactViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -16,8 +36,50 @@ class TransferFailedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transfer_failed, container, false)
+        binding = FragmentTransferFailedBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
 
+        binding.btnContinue.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        viewModel.getSelectedContact().observe(viewLifecycleOwner) {
+            binding.contactName.text = it.name
+            binding.contactNumber.text = it.phone
+            Glide.with(binding.imageContact).load(BASE_URL + it?.image)
+                .apply(
+                    RequestOptions.circleCropTransform()
+                        .placeholder(R.drawable.rumbling)
+                )
+                .into(binding.imageContact)
+        }
+
+        viewModel.getAmount().observe(viewLifecycleOwner) {
+            binding.transferAmount.text = it.amount.toString()
+            binding.balanceAmount.text = "Masih bingung ngitungnya"
+            // format date
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val current = LocalDateTime.now()
+                val formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy - HH:mma")
+                val answer = current.format(formatter)
+                binding.dateTime.text = answer
+            } else {
+                val date = Date()
+                val formatter = SimpleDateFormat("MMM dd, yyyy - HH:mma")
+                val answer = formatter.format(date)
+                binding.dateTime.text = answer
+            }
+            if (it.notes.isEmpty()) {
+                binding.notes.text = "-"
+            } else {
+                binding.notes.text = it.notes
+            }
+
+        }
+    }
 }
--- app/src/main/java/com/yudhinurb/zwallet/ui/layout/main/transfersuccess/TransferSuccessFragment.kt
+++ app/src/main/java/com/yudhinurb/zwallet/ui/layout/main/transfersuccess/TransferSuccessFragment.kt
@@ -1,6 +1,7 @@
 package com.yudhinurb.zwallet.ui.layout.main.transfersuccess
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
@@ -8,12 +9,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.yudhinurb.zwallet.R
 import com.yudhinurb.zwallet.databinding.FragmentTransferSuccessBinding
 import com.yudhinurb.zwallet.ui.layout.main.MainActivity
+import com.yudhinurb.zwallet.ui.layout.main.findreceiver.ContactViewModel
+import com.yudhinurb.zwallet.utils.BASE_URL
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class TransferSuccessFragment : Fragment() {
     private lateinit var binding: FragmentTransferSuccessBinding
+    private val viewModel: ContactViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -34,6 +46,39 @@ class TransferSuccessFragment : Fragment() {
                 val intent = Intent(activity, MainActivity::class.java)
                 startActivity(intent)
                 activity?.finish()}, 2000)
+        }
+
+        viewModel.getSelectedContact().observe(viewLifecycleOwner){
+            binding.contactName.text = it.name
+            binding.contactNumber.text = it.phone
+            Glide.with(binding.imageContact).load(BASE_URL + it?.image)
+                .apply(
+                    RequestOptions.circleCropTransform()
+                        .placeholder(R.drawable.rumbling))
+                .into(binding.imageContact)
+        }
+
+        viewModel.getAmount().observe(viewLifecycleOwner){
+            binding.transferAmount.text = it.amount.toString()
+            binding.balanceAmount.text = "Masih bingung ngitungnya"
+            // format date
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val current = LocalDateTime.now()
+                val formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy - HH:mma")
+                val answer =  current.format(formatter)
+                binding.dateTime.text = answer
+            } else {
+                val date = Date()
+                val formatter = SimpleDateFormat("MMM dd, yyyy - HH:mma")
+                val answer = formatter.format(date)
+                binding.dateTime.text = answer
+            }
+            if (it.notes.isEmpty()) {
+                binding.notes.text = "-"
+            } else {
+                binding.notes.text = it.notes
+            }
+
         }
     }
 
